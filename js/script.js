2448ba341897c48f768182b4a8c3faf660fe248c
mukhtasar93 <mukhtasar03@gmail.com>
2022-02-28 10:04:09 +0700

    add hover

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const container = document.getElementsByTagName("body");
 const kotak = document.getElementsByClassName("kotak");
+const kotakHover = document.getElementsByClassName("kotak-hover");
 // const merah = document.getElementById("red");
 
 // merah.addEventListener("click", function () {
@@ -9,6 +10,13 @@ const kotak = document.getElementsByClassName("kotak");
 for (let i = 0; i < kotak.length; i++) {
 	kotak[i].addEventListener("click", function () {
 		const currentColor = kotak[i].id;
+		container[0].style.backgroundColor = currentColor;
+	});
+}
+
+for (let i = 0; i < kotakHover.length; i++) {
+	kotakHover[i].addEventListener("mouseenter", function () {
+		const currentColor = kotakHover[i].id;
 		container[0].style.backgroundColor = currentColor;
 	});
 }
